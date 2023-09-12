a1cf3744e6acd5f3fe65592ad1df383c5fadcb50
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2023-09-11 23:14:09 -0300

    cambio de la variable a db para poder utilizar el contexto de data source

--- src/shared/infrastructure/config/postgres/Postgres.ts
+++ src/shared/infrastructure/config/postgres/Postgres.ts
@@ -1,4 +1,4 @@
-import {DataSource, EntitySchema} from 'typeorm'
+import {DataSource} from 'typeorm'
 import 'dotenv/config'
 import { error } from 'console';
 import {User} from '../../../../Users/infrastructure/persistence/Postgres/model/UserModel'
@@ -21,32 +21,10 @@ export class Postgres{
        migrations: [],
      });
 
-    //  dbConection: DataSource
  
-    constructor( ){
-      // this.dbConection = new DataSource({
-      //     type: "postgres",
-      //      host:"postgres",
-      //      port: 5432,
-      //      username: `${process.env.POSTGRES_USER}`,
-      //      password: `${process.env.POSTGRES_PASSWORD}`,
-      //      database: `${process.env.POSTGRES_DB}`,
-      //      synchronize: true,
-      //      logging: true,
-      //      entities: [User],
-      //      subscribers: [],
-      //      migrations: [],
-      //    });
-    }
+    constructor( ){}
   
     connection() : void {
-          
-      // this.dbConection.initialize()
-      //     .then(() => log.info(`Successfully connected to database:${this.dbConection.options.database} , type:${this.dbConection.options.type} `))
-      //   // .then(() => console.log(`Successfully connected to ${this.db}`))
-      //     .catch((error) => log.error('Error connecting to database: ', error));
-      //     //.catch((error) => console.log('Error connecting to database: ', error));
-      
       Postgres.db.initialize()
       .then(() => log.info(`Successfully connected to database:${Postgres.db.options.database} , type:${Postgres.db.options.type}  `))
         // .then(() => console.log(`Successfully connected to ${this.db}`))
@@ -58,21 +36,3 @@ export class Postgres{
 
 
 
-// export const db = new DataSource({
-//   type: "postgres",
-//     host: "postgres",
-//     port: 5432,
-//     username: `${process.env.POSTGRES_USER}`,
-//     password: `${process.env.POSTGRES_PASSWORD}`,
-//     database: `${process.env.POSTGRES_DB}`,
-//     synchronize: true,
-//     logging: true,
-//     entities: [],
-//     subscribers: [],
-//     migrations: [],
-// })
-
-
-// connection = (): void =>{
-
-// }

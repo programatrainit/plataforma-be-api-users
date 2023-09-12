6ed1d8e11a40f20d8f75a7865313cc6b70480dd0
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2023-09-11 22:54:19 -0300

    creacion y actualizacion de los metodos create find

--- src/shared/infrastructure/persistence/typeORM/typeOrmBaseRepository.ts
+++ src/shared/infrastructure/persistence/typeORM/typeOrmBaseRepository.ts
@@ -1,51 +1,43 @@
-import {BaseEntity, DataSource , } from  'typeorm'
- import { Write } from '../../../domain/repository/Write';
-import { Read } from '../../../domain/repository/Read';
-import { type } from 'os';
-import { User } from 'Users/infrastructure/persistence/Postgres/model/UserModel'; 
+import {BaseEntity, ObjectLiteral } from  'typeorm'
+import { Write } from '../../../domain/repository/Write';
+import { Postgres } from '../../config/postgres/Postgres';
+import {Read} from '../../../domain/repository/Read'
 
 
-export class TyOrmBaseRepository implements Write{
-  private _model: BaseEntity;
-  // private _dataModel : DataSource;
-  // protected _userRepository: any;
-   
 
-  constructor(schemaModel: BaseEntity) {
-    this._model = schemaModel;
-    // this._userRepository = this._dataModel.getRepository(schemaModel);
+export class TyOrmBaseRepository implements Write , Read{
+  private _model:any;
+
+
+// se hace una injeccion de dependencias para poder utilizar el modelo 
+  constructor( model: any) {
+      this._model= model
   }
 
-   create< V>(body: any ): Promise<V> {
+   create<T, V>(body: T ): Promise<V> {
     return new Promise<V>(async (resolve, reject) => {
-      
         if(body == undefined){
           log.error(`Database error is  ${body}`);
           reject(body);
          
         }else{
-          log.info(`TyOrmBaseRepository  , body : ${body}`)
-          this._model.save(...body)
-          log.info(`Database response ${JSON.stringify(body)}`);
-          resolve(body as unknown as V);
+          // verificamos que el dato se que envio sea del que implemente las entidades 
+          //../../../../Users/infrastructure/persistence/Postgres/model/UserModel'; --> modelo entidad 
+          if(typeof this._model == typeof BaseEntity){
+              Postgres
+              .db
+              .getRepository(this._model)
+              .save(body);
+              log.info(`Database response ${JSON.stringify(body)}`);
+              resolve(body  as V);
+          }
+          else
+          {
+            log.error(`Database error is  ${this._model}`);
+            reject(body);
+          }
+        
         }
-
-
-
-      // log.info(` body : ${JSON.stringify(body)}`)
-      // let save = await this._model.save(body);
-      // log.info(`Database response ${JSON.stringify(save)}`);
-      // resolve( save as unknown as V);
-
-      // this._model.create(body, (err: CallbackError, res: Document) => {
-      //   if (err) {
-      //     log.error(`Database error ${err}`);
-      //     reject(err);
-      //   } else {
-      //     log.info(`Database response ${JSON.stringify(res)}`);
-      //     resolve(res as unknown as V);
-      //   }
-      // });
     });
   }
 
@@ -72,26 +64,25 @@ export class TyOrmBaseRepository implements Write{
   //   });
   // }
 
-  // find<T, V>(
-  //   filter: FilterQuery<T>,
-  //   projection?: never | null,
-  //   options?: QueryOptions,
-  // ): Promise<Array<V>> {
-  //   return new Promise<Array<V>>((resolve, reject) => {
-  //     this._model.find(
-  //       filter,
-  //       projection,
-  //       options,
-  //       (err: CallbackError, res: Array<Document>) => {
-  //         if (err) {
-  //           log.error(`Database error ${err}`);
-  //           reject(err);
-  //         } else {
-  //           log.info(`Database response ${JSON.stringify(res)}`);
-  //           resolve((res as unknown) as Array<V>);
-  //         }
-  //       },
-  //     );
-  //   });
-  // }
-}
+  find<T, V>(): Promise<Array<V>> {
+    return new Promise<Array<V>>((resolve, reject) => {
+
+          if (typeof this._model == typeof BaseEntity) {
+            const users :  Promise<Array<ObjectLiteral>> =
+            Postgres
+            .db
+            .getRepository(this._model)
+            .find();
+            users.then( users => resolve(users as Array<V>))
+            log.info(`Database response ${users}`);
+            // resolve((users as unknown) as Array<V>);
+            // log.error(`Database error ${err}`);
+            // reject(err);
+          } else {
+            log.error(`Database error ${this._model}`);
+            reject();
+          }
+        },);
+    }
+  }
+

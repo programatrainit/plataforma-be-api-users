4c214a4373ce58c0da6e3365e982a4d16b2be855
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2023-09-11 23:24:24 -0300

    cambio en la implementacion del metodo

--- src/Users/infrastructure/controller/CreateUserController.ts
+++ src/Users/infrastructure/controller/CreateUserController.ts
@@ -5,31 +5,41 @@ import { Response, Tags } from 'typescript-rest-swagger';
 import { IBaseController } from '../../../shared/infrastructure/controllers/IBaseController';
 import { ErrorHandler } from '../../../shared/domain/service/ErrorHandler';
 import {IUserCreate } from '../../application/use-case/interface/IUserCreate';
-import { User } from '../persistence/Postgres/model/UserModel'
-import { type } from 'os';
+import {  IUser } from '../../../Users/domain/entity/IUser';
+import {randomUUID} from 'crypto'
 
-@Path('/User')
+@Path('/users')
 export class CreateUserController implements IBaseController {
   private readonly useCase: IUserCreate;
-  // // private  users : User;
 
   constructor(useCase: IUserCreate) {
     this.useCase = useCase;
   }
 
   run = async (req: Request, res: Resp): Promise<void> => {
-    const {nombre, apellido , email ,cv_bucket_url,likedin_url ,github_url }  = req.body;
-    let body = new User();
-    body.nombre = nombre;
-    body.apellido = apellido;
-    body.email = email;
-    body.cv_bucket_url = cv_bucket_url;
-    body.likedin_url = likedin_url;
-    body.github_url =  github_url;
+    
+    const id  = randomUUID()
+    const {nombre , apellido , email ,cv_bucket_url,likedin_url ,github_url }  = req.body;
+
+      log.info(`valores de  nombre ${nombre}`);
+//====== Creacion de Usuario con IUser ======
+    const user : IUser ={
+      id : Object.freeze(id)  ,
+      nombre : nombre,
+      apellido :apellido,
+      email : email,
+      cv_bucket_url: cv_bucket_url,
+      likedin_url:likedin_url,
+      github_url:github_url,
+      created_at:new Date(),
+      updated_at: new Date()
+
+    }
+    
     try {
-      log.info(` user : ${body.nombre}`);
-      const response = await this.impl(body);
-      log.info(` user : ${response}`);
+      // log.info(` user : ${body.nombre}`);
+      const response = await this.impl(user);
+      // log.info(` user : ${response}`);
       res.status(httpStatus.OK).json(response);
     } catch (error) {
       ErrorHandler.catch(error as Record<any, any>, res);
@@ -40,8 +50,8 @@ export class CreateUserController implements IBaseController {
   @Tags('Users')
   @Response<{id: number}>(200, 'OK')
   @Response<{ error: string; }>(503, 'SERVICE UNAVAILABLE')
-  protected async impl(body: User): Promise<{id : number}> { 
-
+  protected async impl(body: IUser): Promise<{id : number}> { 
+       
     return this.useCase.create(body);
   }
 }

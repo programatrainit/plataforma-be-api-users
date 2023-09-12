92fa9a4b7801374e048616135c6bd1e93646b5b4
joskeiner simosa urdaneta <joskeiner.simosa9@gmail.com>
2023-09-11 23:13:14 -0300

    cambio de la variable a db para poder utilizar el contexto de data source

--- src/shared/infrastructure/config/postgres/Postgres.ts
+++ src/shared/infrastructure/config/postgres/Postgres.ts
@@ -6,34 +6,54 @@ import {User} from '../../../../Users/infrastructure/persistence/Postgres/model/
 
 export class Postgres{
 
-    private db : DataSource
+  // se modifico la variable db es static para poder utilizar el contexto de DataSource  
+  static db : DataSource = new DataSource({
+      type: "postgres",
+       host:"postgres",
+       port: 5432,
+       username: `${process.env.POSTGRES_USER}`,
+       password: `${process.env.POSTGRES_PASSWORD}`,
+       database: `${process.env.POSTGRES_DB}`,
+       synchronize: true,
+       logging: true,
+       entities: [User],
+       subscribers: [],
+       migrations: [],
+     });
+
+    //  dbConection: DataSource
  
     constructor( ){
-      this.db = new DataSource({
-          type: "postgres",
-           host:"postgres",
-           port: 5432,
-           username: `${process.env.POSTGRES_USER}`,
-           password: `${process.env.POSTGRES_PASSWORD}`,
-           database: `${process.env.POSTGRES_DB}`,
-           synchronize: true,
-           logging: true,
-           entities: [User],
-           subscribers: [],
-           migrations: [],
-         });
+      // this.dbConection = new DataSource({
+      //     type: "postgres",
+      //      host:"postgres",
+      //      port: 5432,
+      //      username: `${process.env.POSTGRES_USER}`,
+      //      password: `${process.env.POSTGRES_PASSWORD}`,
+      //      database: `${process.env.POSTGRES_DB}`,
+      //      synchronize: true,
+      //      logging: true,
+      //      entities: [User],
+      //      subscribers: [],
+      //      migrations: [],
+      //    });
     }
   
     connection() : void {
           
-      this.db.initialize()
-          .then(() => log.info(`Successfully connected to database:${this.db.options.database} , type:${this.db.options.type} `))
+      // this.dbConection.initialize()
+      //     .then(() => log.info(`Successfully connected to database:${this.dbConection.options.database} , type:${this.dbConection.options.type} `))
+      //   // .then(() => console.log(`Successfully connected to ${this.db}`))
+      //     .catch((error) => log.error('Error connecting to database: ', error));
+      //     //.catch((error) => console.log('Error connecting to database: ', error));
+      
+      Postgres.db.initialize()
+      .then(() => log.info(`Successfully connected to database:${Postgres.db.options.database} , type:${Postgres.db.options.type}  `))
         // .then(() => console.log(`Successfully connected to ${this.db}`))
-          .catch((error) => log.error('Error connecting to database: ', error));
-          //.catch((error) => console.log('Error connecting to database: ', error));
-          
+      .catch((error) => log.error('Error connecting to database: ', error));
     }
-
+    
+  
 }
 
 

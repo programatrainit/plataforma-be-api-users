f1d0537437ce9c41fe3c58fa6c738ba72a5996d2
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-20 16:30:26 -0600

    Update IUser.ts

--- src/Users/domain/entity/IUser.ts
+++ src/Users/domain/entity/IUser.ts
@@ -1,1 +1,12 @@
+// export interface IUser {
+//   id: string;
+//   nombre: string;
+//   apellido: string;
+//   email: string;
+//   cv_bucket_url: string;
+//   github_url: string;
+//   linkedin_url: string;
+//   created_at: Date;
+//   updated_at: Date;
+// }
 

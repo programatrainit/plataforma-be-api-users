046e411f2967ee4a03702d07e182b7dcfb191a8d
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-20 19:25:30 -0600

    Update data-source.ts

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,1 +1,19 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./Users/infrastructure/persistence/Postgres/model/UserModel"
+import { Roles } from "./Roles/infrastruture/persistence/postgres/model/RolModel"
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "0000",
+    database: "train_it_db",
+    synchronize: true,
+    logging: false,
+    entities: [User,Roles],
+    migrations: ["src/migrations/**/*.ts"],
+    subscribers: [],
+})
 

24e643c942e3aa95496582f8038793ba233f6a01
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-11-20 19:30:58 -0600

    Update 1700524612187-added-entity.ts

--- src/migrations/1700524612187-added-entity.ts
+++ src/migrations/1700524612187-added-entity.ts
@@ -1,1 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddedEntity1700524612187 implements MigrationInterface {
+    name = 'AddedEntity1700524612187'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "username" character varying(25) NOT NULL, "password" character varying(25) NOT NULL, "email" character varying(25) NOT NULL, "cv_bucket_url" character varying(25), "github_url" character varying(25), "linkedin_url" character varying(25), "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "roles" ("id" SERIAL NOT NULL, "name" character varying(25) NOT NULL, "description" character varying(25) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_c1433d71a4838793a49dcad46ab" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "roles"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
 

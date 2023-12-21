eca8a3af8e194af88c3e2d7ae3833397b02887ca
sotoJ24 <94422368+sotoJ24@users.noreply.github.com>
2023-12-21 16:00:45 -0600

    Update FindsOneUserCase.test.ts

--- __tests__/unit/use-cases/users/FindsOneUserCase.test.ts
+++ __tests__/unit/use-cases/users/FindsOneUserCase.test.ts
@@ -3,54 +3,56 @@ import { IUser } from '../../../../src/Users/domain/entity/IUser';
 import { UsersRepository } from '../../../../src/Users/infrastructure/persistence/Postgres/UserRepository';
 import { BusinessErrorHandler } from '../../../../src/shared/domain/service/BusinessErrorHandler';
 import { Exception } from '../../../../src/shared/domain/service/Exception';
-import { FindAllUsersUseCase } from '../../../../src/Users/application/use-case/FindAllUserUseCase';
+import { FindOneUserUseCase} from '../../../../src/Users/application/use-case/FindOneUserUseCase';
 import { User } from '../../../../src/Users/infrastructure/persistence/Postgres/model/UserModel';
+
 jest.mock('../../../../src/Users/infrastructure/persistence/Postgres/UserRepository.ts');
 
-describe('RequestFindAllUsersCase', () => {
+describe('RequestFindOneUserUseCase', () => {
+  let idFind: string = '123e4567-e89b-12d3-a456-426614174001';
 
-  const findAllUserResponse = [
-    {
-      id: '123e4567-e89b-12d3-a456-426614174001',
-      nombre: 'Maria',
-      apellido: 'Gonzalez',
-      email: 'maria.Gonzalez@email.com',
-      cv_bucket_url: 'https://fake-s3-bucket.s3.amazonaws.com',
-      github_url: 'https://github.com/fakeuser',
-      linkedin_url: 'https://www.linkedin.com/in/fakeuser',
-      created_at: new Date(),
-      updated_at: new Date()
+  const findOneUserResponse =
+  {
+    id: '123e4567-e89b-12d3-a456-426614174001',
+    nombre: 'Maria',
+    apellido: 'Gonzalez',
+    email: 'maria.Gonzalez@email.com',
+    cv_bucket_url: 'https://fake-s3-bucket.s3.amazonaws.com',
+    github_url: 'https://github.com/fakeuser',
+    linkedin_url: 'https://www.linkedin.com/in/fakeuser',
+    created_at: new Date(),
+    updated_at: new Date()
 
-    }]
+  };
   const userRepository = new UsersRepository (User);
-  const findAllUsersUseCase = new FindAllUsersUseCase(userRepository);
+  const findOneUserUseCase = new FindOneUserUseCase(userRepository);
 
-  let mockUserRepository: jest.SpyInstance<Promise<Array<IUser>>, []>;
+  let mockUserRepository: jest.SpyInstance<Promise<IUser>, [typeof idFind]>;
 
   beforeEach(() => {
     mockUserRepository = jest
-      .spyOn(userRepository, 'findAllUsers')
-      .mockResolvedValue(findAllUserResponse);
+      .spyOn(userRepository, 'findOneUser')
+      .mockResolvedValue(findOneUserResponse);
   });
 
   afterEach(() => {
     mockUserRepository.mockRestore();
   });
 
-  test('find all Users', async () => {
-    const result = await findAllUsersUseCase.findAll();
+  test('find one User', async () => {
+    const result = await findOneUserUseCase.findOne(idFind);
     expect(result).toBeDefined();
-    expect(result).toEqual(findAllUserResponse);
-    expect( mockUserRepository).toHaveBeenCalled();
+    expect(result).toEqual(findOneUserResponse);
+    expect(mockUserRepository).toHaveBeenCalled();
     expect(result).not.toBe(null);
   });
 
-  test('should return error if the find all is not success', async () => {
-    jest.spyOn(findAllUsersUseCase, 'findAll').mockImplementation(() => {
+  test('should return error if the find one is not success', async () => {
+    jest.spyOn(findOneUserUseCase, 'findOne').mockImplementation(() => {
       throw BusinessErrorHandler.createException(new Error('throw error'));
     });
     try {
-      await findAllUsersUseCase.findAll();
+      await findOneUserUseCase.findOne(undefined as unknown as string);
     } catch (err: any) {
       expect(err).toBeInstanceOf(Exception);
       expect(err).toBeInstanceOf(Error);
@@ -60,4 +62,3 @@ describe('RequestFindAllUsersCase', () => {
     }
   });
 });
-
